6854d0d0bfd843d600ceb62597f05ea250522f9a
brysommer <brysommer1@gmail.com>
2025-09-04 13:46:19 +0300

    removed sequalize connection

--- models/sequelize.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-
-export const sequelize = new Sequelize('counter', 'taxibot', '09bot@4516', {
-    host: '49.13.142.186',
-    dialect: 'postgres',
-    port: 5432,  // Default port for PostgreSQL
-    logging: false,  // Disable logging
-});
